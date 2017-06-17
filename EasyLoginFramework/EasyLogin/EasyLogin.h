9602be3b795ddaa769bc54d3e3668bc77fdc35b7
Yoann Gini <yoann.gini@gmail.com>
2017-06-17 19:24:03 +0200

    Refactoring code repo to be linked to an OS and in charge of a dedicated feature

---
+++ EasyLoginFramework/EasyLogin/EasyLogin.h
@@ -0,0 +1,13 @@
+//
+//  EasyLogin.h
+//  EasyLogin
+//
+//  Created by Yoann Gini on 07/06/2017.
+//  Copyright © 2017 EasyLogin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <EasyLogin/EasyLoginDBProtocol.h>
+#import <EasyLogin/EasyLoginDBProxy.h>
+
